e21d443f9679e18a3526f2f6baba3b4c02faade5
abduvosid <vosid071120@g,ail.com>
2021-07-20 09:40:40 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/* let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Alex"; 
+
+const bool = false; */
+
+/* console.log(something); */
+
+/* let und;
+console.log(und)
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}; */
+
+/* console.log(obj.name); */
+/* console.log(obj['name']); */
+
+/* let arr = ['plum.png', 'orrange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1])
+
+const result = confirm('Are you here?');
+console.log(result); */
+
+/* const answer = +prompt('Вам есть 18?', '18');
+console.log(answer + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам ле?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)) */
+
+/* const category = 'toys';
+
+console.loge(`https://someurl.com/${category}/5`); */
+
+/* const user = "Abduvosid";
+
+alert(`Пртвет, ${user}`); */
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+/* ++incr;
+--decr; */
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8); 
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
