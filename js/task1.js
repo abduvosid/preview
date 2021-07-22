fd73093da31606d165e416f95732656ac87e778b
abduvosid <vosid071120@g,ail.com>
2021-07-22 09:04:16 +0500

    task1descr

---
+++ js/task1.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//УСЛОВИЯ
+
+/* if (4 == 1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error'); */
+
+
+/* const num = 50; */
+
+/* switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 51:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+} */
+
+//УСЛОВИЯ И ЦИКЛЫ
+
+
+
+/* while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+let num = 50;
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
